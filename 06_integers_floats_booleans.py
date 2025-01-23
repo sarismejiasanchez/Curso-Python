0a7d5e67e1116e5b648a76878ec418e821eaa959
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2025-01-23 18:25:41 +0000

    6. Enteros, Flotantes y Booleanos

---
+++ 06_integers_floats_booleans.py
@@ -0,0 +1,36 @@
+# 6. Enteros, Flotantes y Booleanos
+
+x = 10
+print(x)
+print(type(x))  # int
+y = 5.432
+print(y)
+print(type(y))  # float
+z = 1.2e6
+print(z)        # 1200000.0
+print(type(z))  # float
+a = 1E6
+print(a)        # 1000000.0
+print(type(a))  # float
+b = 1.2E-6
+print(b)        # 1.2e-06
+print(type(b))  # float
+
+print(x + x)    # 20
+print(x + y)    # 15.432
+print(y + y)    # 10.864
+print(z + a)    # 2200000.0
+
+# Booleans
+is_true = True
+print(is_true)  # True
+print(type(is_true))    # bool
+
+is_false = False
+print(is_false) # False
+print(type(is_false))   # bool
+
+# https://chatgpt.com/share/679288da-0260-8003-8555-6ca278de58ca
+x = 10 
+y = 5.123 
+print(x + y)    # 15.123000000000001
