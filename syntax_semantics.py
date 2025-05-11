2a0d53e75fbb4d5925d1bb1a0bcc61750d206898
sarismejiasanchez <sarismejiasanchez@gmail.com>
2025-05-11 12:08:32 -0500

    3. Semántica y Sintaxis en Programación Python

---
+++ syntax_semantics.py
@@ -0,0 +1,27 @@
+# 3. Semántica y Sintaxis en Programación Python
+
+# Sintax
+
+# SyntaxError: '(' was never closed
+#print("Hello World"
+
+# IndentationError: unexpected indent
+# print("World")
+
+# Semantics
+greeting = "Hello"
+name = "Platzi"
+
+print(greeting, name)
+
+greeting = "Hi"
+name = "Platzi"
+
+print(greeting, name)
+
+# SyntaxError: invalid decimal literal
+#1name = "Platzi"
+
+# SyntaxError: invalid syntax
+# class is a reserved word in Python, it cannot be used to name variables
+#class = "Platzi" 
