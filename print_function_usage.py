3a7949e2731eafefe50e23bcfa8339c2b353cc34
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2025-05-13 15:26:47 +0000

    7. Dominio de la función `print` en Python: usos y formatos avanzados

--- print_function_usage.py
+++ print_function_usage.py
@@ -1,8 +1,8 @@
-# 7. Todo lo que Debes Saber sobre print en Python
+# 7. Dominio de la función print en Python: usos y formatos avanzados
 
 # 1. Uso básico de print
-print("Nunca Pares De Aprender!")
-print("Never Stop Learning!")
+print("Nunca Pares De Aprender!") # Nunca Pares De Aprender!
+print("Never Stop Learning!") # Never Stop Learning!
 
 # 2. Uso de la coma en print
 print("Nunca", "Pares", "De", "Aprender")   # Nunca Pares De Aprender
@@ -23,20 +23,20 @@ print("Nunca", end="/")
 print("pares de aprender")  # Nunca/pares de aprender
 
 # 5. Impresión de variables
-sentence = "Nunca Pares De Aprender"
+phrase = "Nunca Pares De Aprender"
 author = "Platzi"
-print("Frase:", sentence, "Autor:", author) # Frase: Nunca Pares De Aprender Autor: Platzi
+print("Frase:", phrase, "Autor:", author) # Frase: Nunca Pares De Aprender Autor: Platzi
 
 # 6. Uso de formato con f-strings
-sentence = "Nunca Pares De Aprender"
+phrase = "Nunca Pares De Aprender"
 author = "Platzi"
-print(f"Frase: {sentence}, Autor: {author}") # Frase: Nunca Pares De Aprender, Autor: Platzi
+print(f"Frase: {phrase}, Autor: {author}") # Frase: Nunca Pares De Aprender, Autor: Platzi
 
 # 7. Uso de formato con format
-sentence = "Nunca Pares De Aprender"
+phrase = "Nunca Pares De Aprender"
 author = "Platzi"
-print("Frase: {}, Autor: {}".format(sentence, author))  # Frase: Nunca Pares De Aprender, Autor: Platzi
-print("Frase: {}, Autor: {}".format(author, sentence))  # Frase: Platzi, Autor: Nunca Pares De Aprender
+print("Frase: {}, Autor: {}".format(phrase, author))  # Frase: Nunca Pares De Aprender, Autor: Platzi
+print("Frase: {}, Autor: {}".format(author, phrase))  # Frase: Platzi, Autor: Nunca Pares De Aprender
 
 # 8. Impresión con formato específico
 valor = 3.14159
@@ -44,11 +44,14 @@ print("Valor: {:.2f}".format(valor))    # Valor: 3.14
 print("Valor: {:.4f}".format(valor))    # Valor: 3.1416
 
 # 9. Saltos de línea y caracteres especiales
-print(f"{sentence}\n{author}")  # Nunca Pares De Aprender
+print(f"{phrase}\n{author}")    # Nunca Pares De Aprender
                                 # Platzi
 
+# Secuencias de escape para usar comillas simples y dobles
 print("Hola soy \'Sara\'")  # Hola soy 'Sara'
 print("Hola soy \"Sara\"")  # Hola soy "Sara"
+
+# Secuencias de escape para usar barras invertidas
 print("La ruta del repositorio es: \\workspaces\\Curso-Python") # La ruta del repositorio es: \workspaces\Curso-Python\
 
 # SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 25-26: truncated \UXXXXXXXX escape
